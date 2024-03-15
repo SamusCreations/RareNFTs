4e6cbe4265f7dbe1e97629667242faa0343ed177
sebascascanteb <sebas.cascante.b@gmail.com>
2024-03-14 20:18:39 -0600

    Prueba

--- RareNFTs.Arquitecture/Models/User.cs
+++ RareNFTs.Arquitecture/Models/User.cs
@@ -5,7 +5,7 @@ namespace RareNFTs.Infraestructure.Models;
 
 public partial class User
 {
-    public string Id { get; set; } = null!;
+    public string IdUser { get; set; } = null!;
 
     public string? Email { get; set; }
 
