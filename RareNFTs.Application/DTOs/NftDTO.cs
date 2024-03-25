dc12f67b8a295eb6ac89fdcf674cdd36ffbe9742
sebascascanteb <sebas.cascante.b@gmail.com>
2024-03-25 17:49:24 -0600

    craete nft

---
+++ RareNFTs.Application/DTOs/NftDTO.cs
@@ -0,0 +1,45 @@
+﻿using RareNFTs.Infraestructure.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RareNFTs.Application.DTOs;
+
+public record NftDTO
+{
+    [Display(Name = "Id")]
+    [Required(ErrorMessage = "{0} es requerido")]
+    public Guid Id { get; set; }
+
+    [Required(ErrorMessage = "{0} es requerido")]
+    [Display(Name = "Description")]
+    public string? Description { get; set; }
+
+    [Required(ErrorMessage = "{0} es requerido")]
+    [Range(0, 999999999, ErrorMessage = "The minimun price is {0}")]
+    [Display(Name = "Price")]
+    public decimal? Price { get; set; }
+
+    [Display(Name = "Image")]
+    [Required(ErrorMessage = "{0} es requerido")]
+    public byte[] Image { get; set; } = null!;
+
+    [Display(Name = "Date")]
+    [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd}")]
+    public DateTime Date { get; set; } = DateTime.Now;
+
+    [Required(ErrorMessage = "{0} es requerido")]
+    [Display(Name = "Author")]
+    public string? Author { get; set; }
+
+    [Display(Name = "Quantity")]
+    [Range(0, 999999999, ErrorMessage = "The minimum value is {0}")]
+    [Required(ErrorMessage = "{0} es requerido")]
+    public int? Quantity { get; set; }
+
+
+
+}
---
+++ RareNFTs.Infraestructure/Repository/Implementation/RepositoryNft.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RareNFTs.Infraestructure.Data;
+using RareNFTs.Infraestructure.Models;
+using RareNFTs.Infraestructure.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RareNFTs.Infraestructure.Repository.Implementation;
+
+public class RepositoryNft : IRepositoryNft
+{
+    private readonly RareNFTsContext _context;
+
+    public RepositoryNft(RareNFTsContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Guid> AddAsync(Nft entity)
+    {
+        await _context.Set<Nft>().AddAsync(entity);
+        await _context.SaveChangesAsync();
+        return entity.Id;
+    }
+
+ 
+
+    public async Task DeleteAsync(Guid id)
+    {
+        var @object = await FindByIdAsync(id);
+        _context.Remove(@object);
+        _context.SaveChanges();
+    }
+
+  
+    public async Task<ICollection<Nft>> FindByDescriptionAsync(string description)
+    {
+        var collection = await _context
+                                     .Set<Nft>()
+                                     .Where(p => p.Description.Contains(description))
+                                     .ToListAsync();
+        return collection;
+    }
+
+    public async Task<Nft> FindByIdAsync(int id)
+    {
+        var @object = await _context.Set<Nft>().FindAsync(id);
+        return @object!;
+    }
+
+    public Task<Nft> FindByIdAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<ICollection<Nft>> ListAsync()
+    {
+        var collection = await _context.Set<Nft>().
+                                            Include(b => b.ClientNft).
+                                            AsNoTracking().ToListAsync();
+        return collection;
+    }
+
+    public async Task UpdateAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
+
+}
